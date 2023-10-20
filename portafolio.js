cd713f3e3e5397b6dada41ef32da6698139c5167
JUAN ADOLFO DAMIAN DAMIAN <jdamiand@miumg.edu.gt>
2023-10-19 23:40:48 -0600

    portafolio

---
+++ portafolio.js
@@ -0,0 +1,48 @@
+
+
+const llamarexpress = require('express')
+const hbs = require('hbs')
+require('dotenv').config();
+
+const app = llamarexpress()
+const port = process.env.PORT   
+
+
+
+//utilizacion de hbs para el renderizado de la pagina web 
+
+app.set('view engine','hbs')
+
+
+
+/* app.use(llamarexpress.static('./public3/viwes')) */
+//app.use(llamarexpress.static('public'))
+/* Express da soporte a los siguientes métodos de direccionamiento que se corresponden con los métodos HTTP: get, post, put, head,
+delete, options, trace, copy, lock, mkcol, move, purge, propfind, proppatch, unlock, report, mkactivity, checkout, merge, 
+ m-search, notify, subscribe, unsubscribe, patch, search y connect. */
+
+hbs.registerPartials(__dirname +'/views/partials')
+hbs.registerPartial('footer', '{{footer}}');
+hbs.registerPartial('header', '{{header}}');
+
+
+
+app.use(llamarexpress.static('./public'))
+
+app.get('/',  (req, res)=>{
+    res.render('home',{
+        nombre:'Adolfo Damian',
+        title:'CURSO BASICO DE NODE.JS'
+    })
+})
+
+
+app.get('*',  (req, res)=>{
+    res.sendFile(__dirname + '/public/404.html')
+})
+
+// Le indicamos a nuestro servidor que escuche el puerto 8080
+app.listen(port,()=>{
+    console.log(`Escuchando en el puerto http:/localhost:${port}`);
+
+}); 
